307131c0e0cab229104b30669f0d84f055083201
Faraz Ahmed <farazpu@gmail.com>
2017-02-16 22:55:48 -0500

    Adding install script

---
+++ hanelly.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#cd ~
+ 
+#mkdir nakisa
+ 
+#cd nakisa
+ 
+#git clone https://github.com/NakisaInc/docker.git .
+ 
+#sudo bash ./installdocker.sh
+ 
+#cd hanelly/nossl/
+
+clear
+
+echo '
+          _______  _        _______  _        _             
+|\     /|(  ___  )( (    /|(  ____ \( \      ( \   |\     /|
+| )   ( || (   ) ||  \  ( || (    \/| (      | (   ( \   / )
+| (___) || (___) ||   \ | || (__    | |      | |    \ (_) / 
+|  ___  ||  ___  || (\ \) ||  __)   | |      | |     \   /  
+| (   ) || (   ) || | \   || (      | |      | |      ) (   
+| )   ( || )   ( || )  \  || (____/\| (____/\| (____/\| |   
+|/     \||/     \||/    )_)(_______/(_______/(_______/\_/   
+                                                            
+
+
+Please get credentials from Nakisa support. 
+Then login to docker hub using:
+sudo docker login
+
+Then run Hanelly using using:
+sudo docker-compose up -d'
+
