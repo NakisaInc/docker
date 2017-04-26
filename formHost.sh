f6fc5979df9c27c783c41b3e9ad0e5bbced5e2b5
HQ1\JZgierski <jack.zgierski@nakisa.com>
2017-04-26 18:56:04 -0400

    2DiskFormation Installer for Hanelly3.0 (no NGINX)

--- formHost.sh
+++ formHost.sh
@@ -11,9 +11,3 @@ sudo bash ./3-installDocker.sh
 
 # reboot system
 sudo reboot
-
-# instructions for user
-echo "Host formation has been completed - you can take an AMI snapshot if you like"
-echo "Next step is to install the application:"
-echo ""
-echo "sudo bash /nakisa/4-installNakisa.sh"
