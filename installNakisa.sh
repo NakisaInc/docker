2778f4bf68cdaaf531fd0e02bb56d4e67eeed88b
HQ1\JZgierski <jack.zgierski@nakisa.com>
2017-04-26 17:55:16 -0400

    cleanup

--- installNakisa.sh
+++ installNakisa.sh
@@ -5,10 +5,14 @@ cd /nakisa/app
 sudo git clone -b 2DiskFormation https://github.com/NakisaInc/docker.git .
 cd /nakisa/app/hanelly/ssl
 sudo docker login -u devopsnakisa -p nakisa.1
-echo "Application directory is populated and ready for docker-compose"
-echo "Now is a good time to take a Docker Container Startup Ready system snapshot (AWS AMI), before starting the containers."
 echo ""
-echo "Please modify the memory settings in the docker-compose.yml file if you need to and run the following commands:"
+echo "################################################################################"
+echo ""
+echo "#Application directory is populated and ready for docker-compose"
+echo "#Now is a good time to take a Docker Container Startup Ready system snapshot (AWS AMI), before starting the containers."
+echo ""
+echo "#Please modify the memory settings in the docker-compose.yml file if you need to and run the following commands:"
+echo "cd /nakisa/app/hanelly/ssl"
 echo ""
 echo "# start all docker containers"
 echo "sudo docker-compose up -d"
