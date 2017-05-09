ed49a612f6c7a639778381c2161465df97ad725a
Jack Zgierski <JZgierski@hq1.nakisa.net>
2017-05-08 21:07:11 -0400

    cleanup

--- 3-installDocker.sh
+++ 3-installDocker.sh
@@ -20,7 +20,7 @@ sudo apt-get install -y --no-install-recommends \
  
 curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
  
-apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
  
 sudo add-apt-repository \
        "deb https://apt.dockerproject.org/repo/ \
@@ -31,9 +31,9 @@ sudo apt-get update
  
 sudo apt-get -y install docker-engine
  
-curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
  
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
  
 sudo docker-compose --version
 
