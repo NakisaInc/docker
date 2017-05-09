f74342b2f2d557711c30393058722d21fda2f94b
HQ1\JZgierski <jack.zgierski@nakisa.com>
2017-05-09 16:02:32 -0400

    testing

--- formHost.sh
+++ formHost.sh
@@ -8,13 +8,13 @@ sudo apt-get install -y --no-install-recommends git
  
 # create /nakisaInstaller application directory and download the installer
 sudo mkdir /nakisaInstaller; cd /nakisaInstaller
-sudo git clone -b Hanelly_3.0 https://github.com/NakisaInc/docker.git .
+sudo git clone -b Hanelly-3.0 https://github.com/NakisaInc/docker.git .
 
 # partition disk
 #sudo bash ./1-partitionDisk.sh
  
 # configure host
-sudo bash ./2-configureHost.sh
+#sudo bash ./2-configureHost.sh
 
 # install docker service
 #sudo bash ./3-installDocker.sh
