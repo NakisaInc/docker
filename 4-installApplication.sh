804c1c62bd342929c3707a396de3393bd0a33b3b
HQ1\JZgierski <jack.zgierski@nakisa.com>
2017-06-07 22:57:05 -0400

    Basic parametrization for docker userid/password and release version

--- 4-installApplication.sh
+++ 4-installApplication.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-# Must be logged into docker to execute the below
-# sudo docker login -u <docker-userid> -p <docker-password>
+# install Nakisa application docker structure
+cd /nakisa/app
+sudo git clone -b Hanelly-${NAK_RELEASE_TAG} https://github.com/NakisaInc/docker.git .
 
 # create required host directories to persist data and logs
 cd /nakisa/app/hanelly
 sudo mkdir apache-shib; sudo mkdir apache-logs;
 sudo mkdir mysql-data
-sudo mkdir redis-data;  sudo mkdir redis-logs;
+sudo mkdir redis-data;  sudo mkdir redis-logs;               # 3.0.0 CR solution only, deprecated subsequently
 sudo mkdir es-data;     sudo mkdir es-logs;
 sudo mkdir idocs-data;  sudo mkdir idocs-logs;
 sudo mkdir bnr-data;    sudo mkdir bnr-logs;    sudo mkdir bnr-storage;
 sudo mkdir tm-logs
 sudo mkdir grafana
 
-# download all application specific docker images
+# log into docker
+sudo docker login -u ${NAK_DOCKER_ID} -p ${NAK_DOCKER_PW}
+
+# download all application specific docker images for all 3.0.x solutions
 sudo docker swarm init
 sudo docker pull nakisa/apache:2.4-shib                      # based off Apache 2.4.25
 sudo docker pull nginx:1.11.5
@@ -31,10 +35,3 @@ sudo docker pull google/cadvisor:v0.25.0
 sudo docker pull prom/mysqld-exporter:v0.10.0
 sudo docker pull prom/prometheus:v1.6.0
 sudo docker pull grafana/grafana:4.2.0
-
-echo '# when reboot completes complete last steps documented in'
-echo 'cd /nakisa/app; more lastSteps_README'
-
-# reboot system
-sudo rm -rf /nakisaInstaller
-sudo reboot
